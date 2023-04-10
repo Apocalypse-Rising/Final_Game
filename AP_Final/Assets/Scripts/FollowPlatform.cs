8d1a1ff63ada36fe9a571c9fa9865e5579a4d763
danielso1703 <105499304+danielso1703@users.noreply.github.com>
2023-04-10 16:49:11 -0400

    Platform stick

--- AP_Final/Assets/Scripts/FollowPlatform.cs
+++ AP_Final/Assets/Scripts/FollowPlatform.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class FollowPlatform : MonoBehaviour
 {
+    public GameObject player;
+    private bool isPlayerFollowing;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            isPlayerFollowing = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            isPlayerFollowing = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerFollowing)
+        {
+            Vector3 newPosition = player.transform.position;
+            newPosition.x = transform.position.x;
+            player.transform.position = newPosition;
+        }
+    }
 }
